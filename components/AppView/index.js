aaea7d5db7d63b90b6a4a6abb6dd9c71be214125
nathanluu <nate1299@me.com>
2020-11-19 10:32:43 -0800

    Fixed text spacing

--- components/AppView/index.js
+++ components/AppView/index.js
@@ -41,6 +41,7 @@ const AppView = () => {
             return result
         })
     }
+
     function navigateBackwards() {
         setCurrentTrack(prevState => {
             let result
@@ -67,6 +68,7 @@ const AppView = () => {
             newAudio.play()
         }
     }
+
     function stopAudio() {
         if (audio) {
             audio.pause()
@@ -78,32 +80,37 @@ const AppView = () => {
 
     return <div className="full-height flex justify-center items-center">
         <NavigationBar className="fixed top-0 w-full"/>
-        {data && <div className="px-4 text-center">
+        {data && <div className="mx-4 text-center">
             <motion.img animate={audio ? {scale: 1, opacity: 1} : {scale: 0.75, opacity: 0.75}}
                         src={data.album.images[0].url} className="rounded-xl mx-auto"/>
+            <div className="w-64 md:w-auto mx-auto">
+                <h1 className="text-xl md:text-4xl font-bold text-center mt-6 truncate px-4">{data.name}</h1>
+                <h2 className="text-lg md:text-2xl opacity-75 font-light text-center mt-2 truncate px-4 truncate">
+                    {`${data && data.album.name} • ${data.artists[0].name}`}</h2>
+            </div>
 
-            <h1 className="text-2xl md:text-4xl font-bold text-center mt-6 truncate">{data.name}</h1>
-            <h2 className="text-lg md:text-2xl opacity-75 font-light text-center mt-2 truncate">
-                {`${data && data.album.name} • ${data.artists[0].name}`}</h2>
             <div className="flex justify-center space-x-6 items-center mt-6">
                 <button className={styles.circleButton}
                         onClick={navigateBackwards}
                 >
                     <svg className={styles.circleButtonIcon}
                          xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12"/>
                     </svg>
                 </button>
                 <button className={styles.circleButton} onClick={() => audio ? stopAudio() : playAudio(true)}>
                     {audio ? <svg
                         className={styles.circleButtonIcon}
                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg> :<svg
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                              d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                    </svg> : <svg
                         className={styles.circleButtonIcon}
                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                              d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"/>
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
                     </svg>}
 
                 </button>
@@ -112,7 +119,8 @@ const AppView = () => {
                     <svg
                         className={styles.circleButtonIcon}
                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/>
                     </svg>
                 </button>
             </div>
